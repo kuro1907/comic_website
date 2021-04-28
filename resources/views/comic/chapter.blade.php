f5407c8eae4864a44cc1115c3698899e85da5e49
kuro1907 <72237314+kuro1907@users.noreply.github.com>
2021-04-28 15:13:32 +0700

    add storage

--- resources/views/comic/chapter.blade.php
+++ resources/views/comic/chapter.blade.php
@@ -40,27 +40,35 @@
         {!!$chapter->content!!}
     </div>
 </div>
-<div class="container-fruid">
-    <div class="col-md-12 top-50">
-
-        <div class="row justify-content-md-center">
-            <a class="col-md-2 btn btn-dark" href="">Chap trước</a>
-            <div class="col-md-5 ">
-                <select class="form-control" onchange="window.location=this.value;">
-                    @foreach($chapters as $key => $currentChapter)
-                    @if($currentChapter->id == $chapter->id)
-                    <option value="/comic/{{$comic->id}}/chapter/{{$currentChapter->id}}" selected>Chap {{$currentChapter->number}} - {{$currentChapter->name}}</option>
-                    @else
-                    <option value="/comic/{{$comic->id}}/chapter/{{$currentChapter->id}}">Chap {{$currentChapter->number}} - {{$currentChapter->name}}</option>
-                    @endif
-                    @endforeach
-                </select>
-            </div>
-            <a class="col-md-2 btn btn-dark" href="">Chap sau</a>
-
+<div class="col-md-12 top-50">
+    <div class="row justify-content-md-center">
+        <h3>{{$comic->name}} - Chap {{$chapter->number}}: {{$chapter->name}}</h3>
+    </div>
+    @php
+    foreach($chapters as $key => $currentChapter) {
+    if($currentChapter->id == $chapter->id) {
+    $previous_chapter_id = $chapters[$key-1]->id;
+    $next_chapter_id = $chapters[$key+1]->id;
+    }
+    }
+    @endphp
+    <div class="row justify-content-md-center">
+        <a class="col-md-2 btn btn-dark" href="/comic/{{$comic->id}}/chapter/{{$previous_chapter_id}}">Chap trước</a>
+        <div class="col-md-5 ">
+            <select class="form-control" onchange="window.location=this.value;">
+                @foreach($chapters as $key => $currentChapter)
+                @if($currentChapter->id == $chapter->id)
+                <option value="/comic/{{$comic->id}}/chapter/{{$currentChapter->id}}" selected>Chap {{$currentChapter->number}} - {{$currentChapter->name}}</option>
+                @else
+                <option value="/comic/{{$comic->id}}/chapter/{{$currentChapter->id}}">Chap {{$currentChapter->number}} - {{$currentChapter->name}}</option>
+                @endif
+                @endforeach
+            </select>
         </div>
+        <a class="col-md-2 btn btn-dark" href="/comic/{{$comic->id}}/chapter/{{$next_chapter_id}}">Chap sau</a>
 
     </div>
+
 </div>
 
 @endsection
